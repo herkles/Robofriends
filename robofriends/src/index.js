f9df4963877c243a7d0158800fb81f1004ad9298
Herkles <seanpatrickzeitler@gmail.com>
2020-07-16 14:03:25 -0400

    created the cardlist.js file and reformated index.js

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card.js';
+import CardList from './CardList.js';
 import 'tachyons';
 import { robots } from './robots.js';
 
 
-ReactDOM.render(
-  <div>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    <Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
-  </div>
-
-,  document.getElementById('root'));
+ReactDOM.render(<CardList robots={robots} />,  document.getElementById('root'));
