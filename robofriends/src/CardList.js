5c5fb5c5f5a3736606f5588587584e2d188a7287
Herkles <seanpatrickzeitler@gmail.com>
2020-07-16 15:07:48 -0400

    reformated cardlist.js

--- robofriends/src/CardList.js
+++ robofriends/src/CardList.js
@@ -3,9 +3,15 @@ import Card from './Card.js';
 
 const CardList = ({ robots }) => {
   const CardComponent = robots.map((user, i) => {
-    return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
+    return (
+      <Card 
+        key={i} 
+        id={robots[i].id} 
+        name={robots[i].name} 
+        email={robots[i].email}
+      />
+    );
   })
-
   return (
     <div>
       {CardComponent}
