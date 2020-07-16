79bb62564548d2336c852b6bfe1dc04e4af4f853
Herkles <seanpatrickzeitler@gmail.com>
2020-07-16 15:15:23 -0400

    created the app.js file and reformated index.js

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList.js';
+import App from './App.js';
 import 'tachyons';
-import { robots } from './robots.js';
 
 
-ReactDOM.render(<CardList robots={robots} />,  document.getElementById('root'));
+ReactDOM.render(<App />,  document.getElementById('root'));
