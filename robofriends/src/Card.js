67197cbaf7a5fe211972479c53b7c333cdfa69d6
Herkles <seanpatrickzeitler@gmail.com>
2020-07-16 11:12:59 -0400

    updated the Card.js file to account for props and to make it dynamic

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <div className='bg-light-green dib br3 pa3 ma2 grow'>
-      <img alt='robots' src='https://robohash.org/Odin' />
+    <div className='tc bg-light-green dib br3 pa3 ma2 grow'>
+      <img alt='robots' src={`https://robohash.org/${props.id}?200x200`} />
       <div>
-        <h2>Jane Doe</h2>
-        <p>JaneDoe@gmail.com</p>
+        <h2>{props.name}</h2>
+        <p>{props.email}</p>
       </div>
     </div>
   );
